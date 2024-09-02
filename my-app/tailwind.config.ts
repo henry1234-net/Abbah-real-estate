06892441fe82e7ef12047fd245865e2b40d37557
Henry <taiwoolujames@gmail.com>
2024-09-02 07:14:32 +0100

    update tailwind.config.ts

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -8,6 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "abbah-main-green": "#228b22",
+        "abbah-dark-green": "#114411",
+        "abbah-light-green": "#34d334",
+        "abbah-dark-slate": "#101928",
+        "abbah-dark-grey": "#645D5D",
+        "abbah-milk": "#f5f5dc",
+        "abbah-red": "#e23939",
+        "abbah-black": {
+          100: "#000800",
+          200: "#0D0D0D",
+        },
+        "abbah-grey": {
+          100: "#F0F2F5",
+          500: "#667185",
+          600: "#475367",
+          700: "#344054",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
