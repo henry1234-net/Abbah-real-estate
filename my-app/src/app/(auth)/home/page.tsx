f15dfbf07c374963bf0b5c54cc25f96d5742ba9e
Henry <taiwoolujames@gmail.com>
2024-09-06 15:51:38 +0100

    added popular images section in home/page.tsx

--- my-app/src/app/(auth)/home/page.tsx
+++ my-app/src/app/(auth)/home/page.tsx
@@ -4,15 +4,16 @@ import Navigation from '@/components/navbar';
 import Image from 'next/image';
 
 export default function LoginPage() {
+const allImage = ['/HeroImg.png', '/HeroImg.png', '/HeroImg.png', '/HeroImg.png', '/HeroImg.png', '/HeroImg.png', '/HeroImg.png']
   return (
     <div className='p-3 max-w-screen'>
       <Navigation />
-      <div className='bg-abbah-milk/50 flex md:flex-row flex-col relative max-h-screen'>
+      <div className='bg-abbah-milk/50 flex md:flex-row flex-col relative rounded-md'>
         <div className="md:w-1/2 h-full md:mt-[4%] text-left p-4 md:relative">
           <h2 className='text-abbah-black-100 md:text-[40px] text-[30px] font-semibold'>
             Find your way <span className='text-abbah-main-green font-bold'>Home</span> with us
           </h2>
-          <p className='md:text-xl text-base mt-4'>
+          <p className='md:text-xl text-lg mt-4'>
             Your journey to finding the perfect property starts here. Explore our listings to find your dream home or reach out to us for personalized assistance.
           </p>
 
@@ -42,7 +43,7 @@ export default function LoginPage() {
         </div>
         <div className="md:w-1/2 h-fit relative order-first md:order-last">
           <Image
-            src='/HeroImg.png'
+            src={allImage[0]}
             alt='Hero Image'
             layout='responsive'
             width={700}
@@ -51,8 +52,36 @@ export default function LoginPage() {
           />
         </div>
       </div>
-      <div className='w-full h-[80vh] bg-abbah-milk/50 mt-4 p-2 rounded-lg'>
-        How are you
+      <div className='w-full h-fit flex flex-col bg-abbah-milk/50 mt-4 p-2 rounded-lg overflow-hidden'>
+        <span className='text-[9px] text-abbah-light-green'>Best Choice</span>
+        Popular Residences
+
+        <div className='flex flex-row overflow-x-scroll'>
+          {allImage.map((imageSrc, index) =>(
+            <div className='bg-white rounded-md mx-2 relative my-3'>
+              <div key={index} className='bg-abbah-milk rounded-md h-[180px] flex-shrink-0 relative'>
+                <Image
+                  src={imageSrc}
+                  alt={`image ${index + 1}`}
+                  layout='fill'
+                  className='rounded-md'
+                  objectFit='cover'
+                />
+              </div>
+              <div key={index} className='flex flex-col text-xs p-4'>
+                Abbah Estate, kubwa, Abuja
+                <div><span className='text-abbah-light-green'> ₦1,000,000</span>/month</div>
+                <hr className='my-2'/>
+                <div className='flex flex-row justify-between'>
+                  <span className='mx-1'>🛏 3 bedrooms</span>
+                  <span className='mx-1'>🚽 4 bathrooms</span>
+                  <span className='mx-1'>📐 1,000sqm</span>
+                </div>
+              </div>
+            </div>
+          )
+          )}
+        </div>
       </div>
     </div>
   );
